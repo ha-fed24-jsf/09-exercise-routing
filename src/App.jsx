6ab60baa4ff9776970cdcf94e7bc702df8057822
David Andersson <lejonmanen@gmail.com>
2025-03-27 09:24:55 +0100

    Before routing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className="app">
+			<header>
+				header
+			</header>
+			<main>
+				main
+			</main>
+		</div>
+	)
+}
+
+export default App
